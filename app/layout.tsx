4936cf8dbc752158ed74f092a9d00dc96eeb7232
SushankKangane <Kanganesushank36@gmail.com>
2026-02-09 22:24:06 +0530

    updated image symbol

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,19 +36,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/unmute-symbol.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/unmute-symbol.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
+        url: '/unmute-symbol.png',
+        type: 'image/png',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/unmute-symbol.png',
   },
 }
 
